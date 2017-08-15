7be62239e223d204ed8164a60d392802dff59b3a
miyukihappy <3yuki.happy@gmail.com>
2017-08-15 18:10:31 +0900

    誤字を修正。

--- app/src/main/java/jp/techacademy/manabe/miyuki/javalog/Human.java
+++ app/src/main/java/jp/techacademy/manabe/miyuki/javalog/Human.java
@@ -23,7 +23,7 @@ class Human extends Animal implements Movable,Thinkable {
 
     // メンバ関数
     public void say() {
-        Log.d("javatest", "私の名前は" + this.name + "です。年は" + + this.age  + "歳です。");
+        Log.d("javatest", "私の名前は" + this.name + "です。年は" +  this.age  + "歳です。");
     }
 
     @Override
